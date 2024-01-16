a6a6ef558ccaa2a53253f9b0a2f68476fea08876
Felipe Emos <felipe.emos.computacao@gmail.com>
2024-01-15 21:48:46 -0300

    style: prettify some stuff

--- src/navigation/router.ts
+++ src/navigation/router.ts
@@ -1,55 +1,58 @@
 import { RootRoute, Route, Router, useRouter } from "@tanstack/react-router";
 import { BlueTab, GreenTab, IndexTab, YellowTab } from "../screens/tabs";
 import { RootScreen } from "../screens/root";
+import { useCallback } from "react";
 
 const rootRoute = new RootRoute({
   component: RootScreen,
-})
+});
 
 const indexRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/",
   component: IndexTab,
-})
+});
 
 const greenRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/green",
   component: GreenTab,
-})
+});
 
 const blueRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/blue",
   component: BlueTab,
-})
+});
 
 const yellowRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/yellow",
   component: YellowTab,
-})
+});
 
 const routeTree = rootRoute.addChildren([
   indexRoute,
   greenRoute,
   blueRoute,
   yellowRoute,
-])
+]);
 
-export const router = new Router({ routeTree })
+export const router = new Router({ routeTree });
 
-export function useHref() {
+export function useHrefBuilder() {
   const { buildLocation } = useRouter();
-  const hrefBuilder = (
-    ...args: Parameters<(typeof router)["buildLocation"]>
-  ) => buildLocation(...args).href;
 
-  return hrefBuilder;
+  return useCallback(
+    (...args: Parameters<(typeof router)["buildLocation"]>) => {
+      return buildLocation(...args).href;
+    },
+    [buildLocation]
+  );
 }
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
--- src/screens/root.tsx
+++ src/screens/root.tsx
@@ -1,49 +1,49 @@
 import { Button, Link, Tab, Tabs } from "@nextui-org/react";
 import { Outlet, useMatches } from "@tanstack/react-router";
 import { useCallback, useMemo, useState } from "react";
-import { useHref } from "../navigation/router";
+import { useHrefBuilder } from "../navigation/router";
 
 function TabsComponent() {
-  const hrefBuilder = useHref();
+  const hrefBuilder = useHrefBuilder();
 
-  const tabItems = useMemo(() => [
-    {
-      href: hrefBuilder({ to: "/" }),
-      title: "Index",
-    },
-    {
-      href: hrefBuilder({ to: "/green" }),
-      title: "Green",
-    },
-    {
-      href: hrefBuilder({ to: "/blue" }),
-      title: "Blue",
-    },
-    {
-      href: hrefBuilder({ to: "/yellow" }),
-      title: "Yellow",
-    },
-  ] as const, [hrefBuilder]);
+  const tabItems = useMemo(
+    () =>
+      [
+        {
+          href: hrefBuilder({ to: "/" }),
+          title: "Index",
+        },
+        {
+          href: hrefBuilder({ to: "/green" }),
+          title: "Green",
+        },
+        {
+          href: hrefBuilder({ to: "/blue" }),
+          title: "Blue",
+        },
+        {
+          href: hrefBuilder({ to: "/yellow" }),
+          title: "Yellow",
+        },
+      ] as const,
+    [hrefBuilder]
+  );
 
   const matches = useMatches();
 
   const tabsMatchLevel = 1;
-  const selectedKey = matches.length > tabsMatchLevel
-    ? matches[tabsMatchLevel].pathname
-    : undefined;
+  const selectedKey =
+    matches.length > tabsMatchLevel
+      ? matches[tabsMatchLevel].pathname
+      : undefined;
 
-  return <Tabs variant="underlined" selectedKey={selectedKey} size="lg">
-    {
-      tabItems.map(({ href, title }) => (
-        <Tab
-          key={href}
-          title={title}
-          href={href}
-          as={Link}
-        />
-      ))
-    }
-  </Tabs>
+  return (
+    <Tabs variant="underlined" selectedKey={selectedKey} size="lg">
+      {tabItems.map(({ href, title }) => (
+        <Tab key={href} title={title} href={href} as={Link} />
+      ))}
+    </Tabs>
+  );
 }
 
 export function RootScreen() {
@@ -64,18 +64,23 @@ export function RootScreen() {
 
 function RandomNumberRerenderChecker() {
   const [randomNumber, setRandomNumber] = useState(Math.random());
-  const randomize = useCallback((() => setRandomNumber(Math.random())), []);
+  const randomize = useCallback(() => setRandomNumber(Math.random()), []);
 
-  return <div className="p-4 bg-slate-300 w-64 rounded-lg flex flex-col gap-4">
-    <p className="text-xl font-bold">Rerender Checker</p>
-    <p className="text-medium font-medium">
-      Random Number:{" "}
-      <span className="text-red-800 font-bold">
-        {randomNumber.toString().slice(0, 5)}
-      </span>
-    </p>
-    <Button color="primary" onPress={randomize}>Randomize!</Button>
-    <p className="italic">If the Page is being reloaded, then this number will change</p>
-  </div>
+  return (
+    <div className="p-4 bg-slate-300 w-64 rounded-lg flex flex-col gap-4">
+      <p className="text-xl font-bold">Rerender Checker</p>
+      <p className="text-medium font-medium">
+        Random Number:{" "}
+        <span className="text-red-800 font-bold">
+          {randomNumber.toString().slice(0, 5)}
+        </span>
+      </p>
+      <Button color="primary" onPress={randomize}>
+        Randomize!
+      </Button>
+      <p className="italic">
+        If the Page is being reloaded, then this number will change
+      </p>
+    </div>
+  );
 }
-
